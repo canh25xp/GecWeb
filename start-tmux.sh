86801df686aab01c607ecb2640fe19bba20c64d9
Ngô Văn Cảnh <canh25xp@gmail.com>
2025-02-10 19:50:57 +0700

    edit tmux start script

--- start-tmux.sh
+++ start-tmux.sh
@@ -2,14 +2,18 @@
 
 SESSION_NAME="thesis"
 
+# windows 1
 tmux new-session -d -s $SESSION_NAME -n GecWeb -c "$(pwd)"
 
+# windows 2
 tmux new-window -t $SESSION_NAME -n gec-api -c "$(pwd)/api"
-tmux send-keys -t $SESSION_NAME:2 "conda activate gec-api" C-m
+tmux send-keys -t $SESSION_NAME:2 "conda activate gec-api ; clear" C-m
 
+# windows 3
 tmux new-window -t $SESSION_NAME -n gec-web -c "$(pwd)/web"
-tmux send-keys -t $SESSION_NAME:3 "conda activate gec-web" C-m
+tmux send-keys -t $SESSION_NAME:3 "conda activate gec-web ; clear" C-m
 
+# windows 4
 tmux new-window -t $SESSION_NAME -n thesis -c "$(pwd)/report"
 
 tmux select-window -t $SESSION_NAME:1
