9662ecd049df327a7901927f0a2ae821a27f59ed
Ngô Văn Cảnh <canh25xp@gmail.com>
2025-02-14 08:48:43 +0700

    start-tmux: rename windows

--- start-tmux.sh
+++ start-tmux.sh
@@ -6,19 +6,19 @@ SESSION_NAME="thesis"
 tmux new-session -d -s $SESSION_NAME -n GecWeb -c "$(pwd)"
 
 # windows 2
-tmux new-window -t $SESSION_NAME -n gec-api -c "$(pwd)/api"
+tmux new-window -t $SESSION_NAME -n api -c "$(pwd)/api"
 tmux send-keys -t $SESSION_NAME:2 "conda activate gec-api ; clear" C-m
 
 # windows 3
-tmux new-window -t $SESSION_NAME -n gec-web -c "$(pwd)/web"
+tmux new-window -t $SESSION_NAME -n web -c "$(pwd)/web"
 tmux send-keys -t $SESSION_NAME:3 "conda activate gec-web ; clear" C-m
 
 # windows 4
-tmux new-window -t $SESSION_NAME -n gec-demo -c "$(pwd)/demo"
+tmux new-window -t $SESSION_NAME -n demo -c "$(pwd)/demo"
 tmux send-keys -t $SESSION_NAME:4 "conda activate gec-demo ; clear" C-m
 
 # windows 5
-tmux new-window -t $SESSION_NAME -n thesis -c "$(pwd)/report"
+tmux new-window -t $SESSION_NAME -n report -c "$(pwd)/report"
 
 tmux select-window -t $SESSION_NAME:1
 
